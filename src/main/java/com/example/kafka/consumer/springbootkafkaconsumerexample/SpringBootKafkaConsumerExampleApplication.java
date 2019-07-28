fe1bf5122349baeeee5b72fb0bcf9cb7bcb4aa6c
tung vo <vomtung@gmail.com>
2019-07-28 17:55:00 +0700

    init project

---
+++ src/main/java/com/example/kafka/consumer/springbootkafkaconsumerexample/SpringBootKafkaConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.consumer.springbootkafkaconsumerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumerExampleApplication.class, args);
+	}
+
+}
