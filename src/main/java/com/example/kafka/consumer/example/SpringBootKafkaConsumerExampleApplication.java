379fdf06986996bff5ba957b2a2ca411fa9b00b1
tung vo <vomtung@gmail.com>
2019-07-28 18:56:52 +0700

    rename package

--- src/main/java/com/example/kafka/consumer/example/SpringBootKafkaConsumerExampleApplication.java
+++ src/main/java/com/example/kafka/consumer/example/SpringBootKafkaConsumerExampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.kafka.consumer.springbootkafkaconsumerexample;
+package com.example.kafka.consumer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
